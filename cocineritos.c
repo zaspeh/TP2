f231c3f43dd559d9993b213a5d7c107473914b61
Matias Kaled Dib <129854973+zaspeh@users.noreply.github.com>
2023-04-22 18:15:07 -0300

    Add files via upload
    Añadì hornos, cuchillos y las herramientas en general.

--- cocineritos.c
+++ cocineritos.c
@@ -11,6 +11,8 @@ const char PARED = '#';
 const char MESA = '_';
 const char SALIDA = 'S';
 const char AGUJERO = 'A';
+const char CUCHILLO = 'C';
+const char HORNO = 'H';
 
 // pre:
 // pos: crea la matriz 21x21 con sus respectivas paredes, piso, mesa y salida.
@@ -37,12 +39,13 @@ void inicializar_nivel(char nivel[MAX_FIL][MAX_COL]) {
       }
     }
     nivel[10][10] = MESA;
-    nivel[21][10] = SALIDA;
+    nivel[20][10] = SALIDA;
 }
 
 void añadir_obstaculos(char nivel[MAX_FIL][MAX_COL]) {
   int cant_agujeros_arriba = 0;
- 
+  int cant_agujeros_abajo = 0;
+
    //añado 10 obstaculos aleatorios en el cuadrante superior
     for (int i = 0; i < 11; i++){
     for (int j = 0; j < MAX_COL; j++){
@@ -59,7 +62,7 @@ void añadir_obstaculos(char nivel[MAX_FIL][MAX_COL]) {
     }
   }
  }
-int cant_agujeros_abajo = 0;
+
 // añado 10 obstaculos aleatorios en el cuadrante inferior
  for (int i = 11; i < 21; i++){
   for (int j = 0; j < MAX_COL; j++){
@@ -71,8 +74,6 @@ int cant_agujeros_abajo = 0;
       nivel[num_fila_aleatorio][num_col_aleatorio] = AGUJERO;
       cant_agujeros_abajo += 1;
       }
-
-      
     }
   }
  }
@@ -80,6 +81,43 @@ int cant_agujeros_abajo = 0;
 
 }
 
+void añadir_herramientas(char nivel[MAX_FIL][MAX_COL]){
+ int cant_cuchillos = 0;
+ int cant_hornos = 0;
+
+// añado 2 cuchillos de forma aleatoria  en el cuadrante superior
+   for (int i = 0; i < 11; i++){
+  for (int j = 0; j < MAX_COL; j++){
+ if(cant_cuchillos < 2) {
+        int num_fila_aleatorio = rand() % 10 + 0;
+        int num_col_aleatorio = rand() % 20 + 0;
+
+   if(nivel[num_fila_aleatorio][num_col_aleatorio] != PARED && nivel[num_fila_aleatorio][num_col_aleatorio] != AGUJERO){
+      nivel[num_fila_aleatorio][num_col_aleatorio] = CUCHILLO;
+      cant_cuchillos += 1;
+      }
+    }
+  }
+ }
+
+// añado 2 hornos de forma aleatoria en el cuadrante inferior
+  for (int i = 11; i < 21; i++){
+  for (int j = 0; j < MAX_COL; j++){
+ if(cant_hornos < 2) {
+        int num_fila_aleatorio = rand() % 10 + 11;
+        int num_col_aleatorio = rand() % 20 + 0;
+
+   if(nivel[num_fila_aleatorio][num_col_aleatorio] != PARED && nivel[num_fila_aleatorio][num_col_aleatorio] != AGUJERO && nivel[num_fila_aleatorio][num_col_aleatorio] != CUCHILLO){
+      nivel[num_fila_aleatorio][num_col_aleatorio] = HORNO;
+      cant_hornos += 1;
+      }
+    }
+  }
+ }
+
+
+
+}
 
 
 
@@ -100,7 +138,7 @@ int main() {
   char nivel[MAX_FIL][MAX_COL];
   inicializar_nivel(nivel);
   añadir_obstaculos(nivel);
-
+  añadir_herramientas(nivel);
 
 
 
